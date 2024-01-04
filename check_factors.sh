f7ac289bf2018a7a75ceeae9b9443ee59e86f5c3
Andrew <49104171+carsonak@users.noreply.github.com>
2024-01-04 23:00:55 +0300

    Moved files into factors

--- check_factors.sh
+++ check_factors.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Time the program and terminate it after 5 seconds, store output in file
-time timeout --preserve-status 4.99 ./factors ./tests/factorising/test00 > out00
+time timeout --preserve-status 4.99 ./factors/factors ./tests/factorising/test00 > out00
 echo $?
 wc -l out00
-time timeout --preserve-status 4.99 ./factors ./tests/factorising/test01 > out01
+time timeout --preserve-status 4.99 ./factors/factors ./tests/factorising/test01 > out01
 echo $?
 wc -l out01
-time timeout --preserve-status 4.99 ./factors ./tests/factorising/test02 > out02
+time timeout --preserve-status 4.99 ./factors/factors ./tests/factorising/test02 > out02
 echo $?
 wc -l out02
-time timeout --preserve-status 4.99 ./factors ./tests/factorising/test03 > out03
+time timeout --preserve-status 4.99 ./factors/factors ./tests/factorising/test03 > out03
 echo $?
 wc -l out03
-time timeout --preserve-status 4.99 ./factors ./tests/factorising/test04 > out04
+time timeout --preserve-status 4.99 ./factors/factors ./tests/factorising/test04 > out04
 echo $?
 wc -l out04
-time timeout --preserve-status 4.99 ./factors ./tests/factorising/test05 > out05
+time timeout --preserve-status 4.99 ./factors/factors ./tests/factorising/test05 > out05
 echo $?
 wc -l out05
-time timeout --preserve-status 4.99 ./factors ./tests/factorising/test06 > out06
+time timeout --preserve-status 4.99 ./factors/factors ./tests/factorising/test06 > out06
 echo $?
 wc -l out06
--- factors_script.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-#timeout factors after 5 seconds
-timeout --preserve-status 5 ./task_0/factors $1
