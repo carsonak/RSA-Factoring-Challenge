9a6214ec0aca451ce06ff4329e1e6f7f8121dba1
Andrew <49104171+carsonak@users.noreply.github.com>
2024-01-05 01:23:09 +0300

    kill -9

--- check_factors.sh
+++ check_factors.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # Time the program and terminate it after 5 seconds, store output in file
-time timeout --preserve-status 4.99 ./task_0/factors ./tests/factorising/test00 > out00.txt
+time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test00 > out00.txt
 echo $?
 wc -l out00.txt
-time timeout --preserve-status 4.99 ./task_0/factors ./tests/factorising/test01 > out01.txt
+time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test01 > out01.txt
 echo $?
 wc -l out01.txt
-time timeout --preserve-status 4.99 ./task_0/factors ./tests/factorising/test02 > out02.txt
+time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test02 > out02.txt
 echo $?
 wc -l out02.txt
-time timeout --preserve-status 4.99 ./task_0/factors ./tests/factorising/test03 > out03.txt
+time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test03 > out03.txt
 echo $?
 wc -l out03.txt
-time timeout --preserve-status 4.99 ./task_0/factors ./tests/factorising/test04 > out04.txt
+time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test04 > out04.txt
 echo $?
 wc -l out04.txt
-time timeout --preserve-status 4.99 ./task_0/factors ./tests/factorising/test05 > out05.txt
+time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test05 > out05.txt
 echo $?
 wc -l out05.txt
-time timeout --preserve-status 4.99 ./task_0/factors ./tests/factorising/test06 > out06.txt
+time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test06 > out06.txt
 echo $?
 wc -l out06.txt
