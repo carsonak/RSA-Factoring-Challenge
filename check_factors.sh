4cf06021a1609ea4b218b3f8993557edddb91c23
Andrew <49104171+carsonak@users.noreply.github.com>
2024-01-08 13:28:46 +0300

    Changed checker script

--- check_factors.sh
+++ check_factors.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
-# Time the program and terminate it after 5 seconds, store output in file
-time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test00 > out00.txt
-echo $?
-wc -l out00.txt
-time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test01 > out01.txt
-echo $?
-wc -l out01.txt
-time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test02 > out02.txt
-echo $?
-wc -l out02.txt
-time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test03 > out03.txt
-echo $?
-wc -l out03.txt
-time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test04 > out04.txt
-echo $?
-wc -l out04.txt
-time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test05 > out05.txt
-echo $?
-wc -l out05.txt
-time timeout -s KILL --preserve-status 4.99 ./factors ./tests/factorising/test06 > out06.txt
-echo $?
-wc -l out06.txt
+program_path="$1" # Program to run
+tests_dir="./tests/factorising/" # Arguments to the program
+out_dir="./" # Directory for the output directories
+
+#Strip trailing slashes
+strip_tslash(){
+    if [[ "$1" = *"/" ]]; then
+        echo "${1%/}"
+    else
+        echo "$1"
+    fi
+}
+
+# Confirm directories exist
+check_dir(){
+    if [[ $# -ge 2 ]]; then
+        for dir in "$@"; do
+            if [[ -d "$dir" ]]; then
+                :
+            else
+                printf "Invalid directory: %s\n" "$dir" >&2
+                return 1
+            fi
+        done
+    else
+        echo "Not enough arguments" >&2
+        return 1
+    fi
+
+    return 0
+}
+
+tests_dir=$(strip_tslash "$tests_dir")
+out_dir=$(strip_tslash "$out_dir")
+
+if check_dir "$tests_dir" "$out_dir" && [[ -r "$tests_dir" && -w "$out_dir" ]]; then
+    # Iterate through all files in the directory
+    for file in "$tests_dir/"*; do
+        out_file="$out_dir/out_$(basename "$file" ".*").txt"
+        # Time the program, terminate after 5 seconds, store its output in file
+        time timeout -s TERM --preserve-status 4.99 "$program_path" "$file" > "$out_file"
+        echo "Status: $?"
+        wc -l "$out_file"
+    done
+fi
