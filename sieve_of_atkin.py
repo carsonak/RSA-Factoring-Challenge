c116a9d77e2b02b85e10979be2a223c645ae36f5
Andrew <49104171+carsonak@users.noreply.github.com>
2024-09-03 21:26:59 +0300

    Removed repeated calculations in sieve of atkins

--- sieve_of_atkin.py
+++ sieve_of_atkin.py
@@ -3,43 +3,106 @@
 # https://en.wikipedia.org/wiki/Sieve_of_Atkin
 # https://www.geeksforgeeks.org/sieve-of-atkin/
 
+# import line_profiler
+import math
 import numpy as np
-from typing import List
+from typing import Literal
 
 
-def sieve_of_atkin(limit: int) -> List[int]:
+def _get_numpy_type(n: int) -> type[np.unsignedinteger]:
+    """Returns the smallest numpy data type that can hold n.
+
+    Args:
+        n: an integer.
+
+    Returns:
+        An instance of numpy.dtype.
+
+    Raises:
+        ValueError if n is greater than 2^128 - 1.
+    """
+    if n >= 2**128:
+        raise ValueError(f"value out of range: {n:40}"
+                         f"{"..." if n >= 10**40 else ""}")
+
+    if n < 2**8:
+        return np.uint8
+
+    if n < 2**16:
+        return np.uint16
+
+    if n < 2**32:
+        return np.uint32
+
+    if n < 2**64:
+        return np.uint64
+
+    return np.uint128
+
+
+def _sieve_to_ndarray(s: np.ndarray[Literal[1], bool]
+                      ) -> np.ndarray[Literal[1], np.unsignedinteger]:
+    """Converts a sieve of primes to a numpy array."""
+    primes_count: int = 0
+    largest_prime = 0
+    for n, is_prime in enumerate(s):
+        if is_prime:
+            largest_prime = n
+            primes_count += 1
+
+    primes = np.empty(primes_count, dtype=_get_numpy_type(largest_prime))
+    primes_count = 0
+    for n, is_prime in enumerate(s):
+        if is_prime:
+            primes[primes_count] = n
+            primes_count += 1
+
+    return primes
+
+
+# @line_profiler.profile
+def sieve_of_atkin(limit: int) -> np.ndarray[Literal[1], np.unsignedinteger]:
     """Returns a list of primes from 0 to limit (inclusive).
 
     Args:
         limit: upper range of primes to find.
 
-    Return:
+    Returns:
         A list of primes from 0 to limit.
+
+    Raises:
+        TypeError: if limit is not an int.
+        ValueError: if limit does not satisfy: 0 <= limit < 2**64.
     """
-    primes: List[int] = []
-    if limit >= 2:
-        primes.append(2)
+    if type(limit) is not int:
+        raise TypeError("limit should be an int type")
 
-    if limit >= 3:
-        primes.append(3)
+    if limit < 0:
+        raise ValueError("limit cannot be a negative integer")
 
-    sieve = np.zeros(limit + 1, np.bool)
+    if limit >= 2**64:
+        raise ValueError("limit is greater than 2^64")
+
+    sieve = np.zeros(limit + 1, dtype=bool)
+    limit_sqrt = int(math.ceil(math.sqrt(limit)))
     x = 1
-    while x**2 <= limit:
+    while x <= limit_sqrt:
         y = 1
-        while y**2 <= limit:
-            n = (4 * x**2) + y**2
-            # (n % 60 == {1,13,17,29,37,41,49,53}) == (n % 12 == {1,5})
+        x_sqr = x**2
+        while y <= limit_sqrt:
+            y_sqr = y**2
+            n = (4 * x_sqr) + y_sqr
+            # (n % 60 == {1,13,17,29,37,41,49,53}) same as (n % 12 == {1,5})
             if n <= limit and (n % 12 == 1 or n % 12 == 5):
                 sieve[n] = not sieve[n]
 
-            n = (3 * x**2) + y**2
-            # (n % 60 == {7,19,31,43}) == (((n % 60) % 12) == n % 12 == {7})
+            n = (3 * x_sqr) + y_sqr
+            # (n % 60 == {7,19,31,43}) same as (n % 12 == {7})
             if n <= limit and n % 12 == 7:
                 sieve[n] = not sieve[n]
 
-            n = (3 * x**2) - y**2
-            # (n % 60 == {11,23,47,59}) == (((n % 60) % 12) == n % 12 == {11})
+            n = (3 * x_sqr) - y_sqr
+            # (n % 60 == {11,23,47,59}) same as (n % 12 == {11})
             if n <= limit and x > y and n % 12 == 11:
                 sieve[n] = not sieve[n]
 
@@ -48,7 +111,7 @@ def sieve_of_atkin(limit: int) -> List[int]:
         x += 1
 
     r = 5
-    while r**2 <= limit:
+    while r <= limit_sqrt:
         if sieve[r]:
             # If r is prime, mark all multiples of its square false.
             for i in range(r**2, limit + 1, r**2):
@@ -56,10 +119,18 @@ def sieve_of_atkin(limit: int) -> List[int]:
 
         r += 1
 
-    primes += [x for x in range(5, limit + 1) if sieve[x]]
-    return primes
+    if limit > 2:
+        sieve[2] = True
+
+    if limit > 3:
+        sieve[3] = True
+
+    return _sieve_to_ndarray(sieve)
 
 
 if __name__ == "__main__":
-    p = sieve_of_atkin(2 ** 32)
-    print(f"\n{len(p)}")
+    import sys
+    n = 2 ** 16
+    p = sieve_of_atkin(n)
+    print(f"Primes in {n}: {len(p)}")
+    print(f"Size of Array: {sys.getsizeof(p)}")
